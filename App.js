321c78bc88f7c28f09c9c99920a4f127d1b92ab1
Bardan P <me@bardanp.com>
2024-01-23 12:21:08 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const events = [
+  { id: '1', title: 'Event 1', description: 'Event 1 Description' },
+  { id: '2', title: 'Event 2', description: 'Event 2 Description' },
+];
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenContainer}>
+      <FlatList
+        data={events}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.eventItem}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
+function NotificationsScreen() {
+  return (
+    <View style={styles.screenContainer}>
+      <Text>Notifications Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={styles.screenContainer}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ tabBarActiveTintColor: '#007bff', tabBarInactiveTintColor: '#a9a9a9',}}>
+        <Tab.Screen name="Events" component={HomeScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignContent: 'center',
+    padding: 5,
+  },
+  eventItem: {
+    backgroundColor: 'lightcoral',
+    padding: 20,
+    marginVertical: 4,
+    marginHorizontal: 15,
+    borderRadius: 15,
+    paddingTop: 10,
+    marginTop: 5,
+  },
+  title: {
+    fontSize: 30,
+    color: 'black',
+    height: 40,
   },
 });
