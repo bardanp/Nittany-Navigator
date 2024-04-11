c4a6ba7a601c9b7841b49fdebc523c6d5dd8cce9
Bardan P <me@bardanp.com>
2024-04-10 20:34:38 -0400

    Updated the addnewevent to select a location o nthe map isnstead of chosing form options.json file

--- screens/addViews/AddNewEvent.js
+++ screens/addViews/AddNewEvent.js
@@ -12,8 +12,9 @@ import {
   TouchableWithoutFeedback,
   FlatList,
   Image,
+  SafeAreaView,
 } from 'react-native';
-
+import MapView, { Marker } from 'react-native-maps';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import * as ImagePicker from 'expo-image-picker';
 import { auth, firestore, storage } from '../../backend/firebase';
@@ -28,7 +29,6 @@ const AddNewEvent = () => {
   const [description, setDescription] = useState('');
   const [image, setImage] = useState(null);
   const [date, setDate] = useState(null);
-  const [locationId, setLocationId] = useState(null);
   const [showLocationPicker, setShowLocationPicker] = useState(false);
   const [organizer, setOrganizer] = useState('');
   const [contactEmail, setContactEmail] = useState('');
@@ -38,7 +38,19 @@ const AddNewEvent = () => {
   const [showCategoryPicker, setShowCategoryPicker] = useState(false);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [createdBy, setCreatedBy] = useState('');
+  const [selectedLocation, setSelectedLocation] = useState(null);
   const navigation = useNavigation();
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [locationDetails, setLocationDetails] = useState('');
+
+
+  const [region, setRegion] = useState({
+    latitude: 40.204444839295846,
+    longitude: -76.74518002144552,
+    latitudeDelta: 0.04,
+    longitudeDelta: 0.05,
+  });
 
   useEffect(() => {
     (async () => {
@@ -70,6 +82,12 @@ const AddNewEvent = () => {
       { text: 'Choose from Gallery', onPress: handleChooseFromGallery },
       { text: 'Cancel', style: 'cancel' },
     ]);
+  };
+
+  const handleMapPress = (e) => {
+    const { latitude, longitude } = e.nativeEvent.coordinate;
+    setSelectedLocation({ latitude, longitude });
+    setShowLocationPicker(false); 
   };
 
   const handleTakePhoto = async () => {
@@ -118,60 +136,60 @@ const AddNewEvent = () => {
         <Image source={{ uri: image }} style={styles.image} resizeMode="cover" />
       );
     }
-      return null;
-    };
+    return null;
+  };
   
-    const clearImage = () => {
-      setImage(null);
-    };
-  
+  const clearImage = () => {
+    setImage(null);
+  };
 
-    const handleSubmit = async () => {
-      console.log('Submitting event...');
-      if (!title || !description || !locationId || !categoryId || !createdBy) {
-        Alert.alert('Missing Information', 'Please fill in all required fields.');
-        return; 
+  const handleSubmit = async () => {
+    console.log('Submitting event...');
+    if (!title || !description || !selectedLocation || !categoryId || !createdBy) {
+      Alert.alert('Missing Information', 'Please fill in all required fields.');
+      return; 
+    }
+
+    const selectedCategory = options.categories.find(cat => cat.id === categoryId);
+    if (!selectedCategory) {
+      Alert.alert('Invalid Selection', 'Please select a valid category.');
+      return;
+    }
+
+    try {
+      let imageUrl = '';
+      if (image) {
+        const response = await fetch(image);
+        const blob = await response.blob();
+        const imageName = `event_images/${Date.now()}`;
+        const imageRef = ref(storage, imageName);
+        const snapshot = await uploadBytes(imageRef, blob);
+        imageUrl = await getDownloadURL(snapshot.ref);
       }
-    
-      const selectedLocation = options.locations.find(loc => loc.id === locationId);
-      const selectedCategory = options.categories.find(cat => cat.id === categoryId);
-      if (!selectedLocation || !selectedCategory) {
-        Alert.alert('Invalid Selection', 'Please select a valid location and category.');
-      }
-    
-      try {
-        let imageUrl = '';
-        if (image) {
-          const response = await fetch(image);
-          const blob = await response.blob();
-          const imageName = `event_images/${Date.now()}`;
-          const imageRef = ref(storage, imageName);
-          const snapshot = await uploadBytes(imageRef, blob);
-          imageUrl = await getDownloadURL(snapshot.ref);
-        }
-    
-        const eventInfo = {
-          title,
-          description,
-          image: imageUrl,
-          dateTime: date ? Timestamp.fromMillis(date.getTime()) : null,
-          location: selectedLocation.name,
-          organizer,
-          contactEmail,
-          rsvpCount,
-          category: selectedCategory.name,
-          submittedOn: Timestamp.now(),
-          createdBy,
-        };
-    
-        await addDoc(collection(firestore, 'events'), eventInfo);
-        Alert.alert('Success', 'Event added successfully!');
-        navigation.goBack();
-      } catch (error) {
-        Alert.alert('Error', 'Failed to add the event.');
-      }
-    };
-    
+
+      const eventInfo = {
+        title,
+        description,
+        image: imageUrl,
+        dateTime: date ? Timestamp.fromMillis(date.getTime()) : null,
+        locationCords: selectedLocation ? `${selectedLocation.latitude}, ${selectedLocation.longitude}` : null,
+        locationDetails, 
+        organizer,
+        contactEmail,
+        rsvpCount,
+        category: selectedCategory.name,
+        submittedOn: Timestamp.now(),
+        createdBy,
+      };
+      
+
+      await addDoc(collection(firestore, 'events'), eventInfo);
+      Alert.alert('Success', 'Event added successfully!');
+      navigation.goBack();
+    } catch (error) {
+      Alert.alert('Error', 'Failed to add the event.');
+    }
+  };
 
   const renderSelectedDate = () => {
     if (date) {
@@ -180,13 +198,31 @@ const AddNewEvent = () => {
     return null;
   };
 
-  const renderSelectedLocation = () => {
-    const selectedLocation = locationId ? options.locations.find((loc) => loc.id === locationId) : null;
-    if (selectedLocation) {
-      return <Text style={styles.selectedText}>Selected Location: {selectedLocation.name}</Text>;
-    }
-    return null;
+  const renderLocationSelector = () => {
+    return (
+    <View style={styles.actionContainer}>
+      <Text style={styles.actionText} onPress={() => setShowLocationPicker(true)}>
+        Select Location
+      </Text>
+      {selectedLocation && (
+        <>
+          <Text style={styles.locationText}>
+            {`Latitude: ${selectedLocation.latitude.toFixed(5)}, Longitude: ${selectedLocation.longitude.toFixed(5)}`}
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Location Details: e.g., Olmstead, Room number, etc (optional)"
+            onChangeText={setLocationDetails}
+            value={locationDetails}
+            placeholderTextColor="#999"
+          />
+        </>
+      )}
+    </View>
+
+    );
   };
+  
 
   const renderSelectedCategory = () => {
     const selectedCategory = categoryId ? options.categories.find((cat) => cat.id === categoryId) : null;
@@ -214,7 +250,7 @@ const AddNewEvent = () => {
         value={description}
         placeholderTextColor="#999"
       />
-            <TextInput
+      <TextInput
         style={styles.input}
         placeholder="Organizer"
         onChangeText={setOrganizer}
@@ -233,6 +269,7 @@ const AddNewEvent = () => {
           Pick Date & Time
         </Text>
         {renderSelectedDate()}
+        {renderLocationSelector()}
       </View>
       <DateTimePickerModal
         isVisible={showDatePicker}
@@ -243,46 +280,44 @@ const AddNewEvent = () => {
         }}
         onCancel={() => setShowDatePicker(false)}
       />
-      <View style={styles.actionContainer}>
-        <Text style={styles.actionText} onPress={() => setShowLocationPicker(true)}>
-          Select Location
-        </Text>
-        {renderSelectedLocation()}
-      </View>
       <Modal
         visible={showLocationPicker}
         animationType="slide"
         transparent={false}
         onRequestClose={() => setShowLocationPicker(false)}
       >
-        <TouchableWithoutFeedback onPress={() => setShowLocationPicker(false)}>
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalTitle}>Select Location</Text>
-              <FlatList
-                data={options.locations}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                  <TouchableWithoutFeedback
-                    onPress={() => {
-                      setLocationId(item.id);
-                      setShowLocationPicker(false);
-                    }}
-                  >
-                    <Text style={styles.item}>{item.name}</Text>
-                  </TouchableWithoutFeedback>
-                )}
-              />
-              <TouchableWithoutFeedback onPress={() => setShowLocationPicker(false)}>
-                <View style={[styles.modalButton, styles.closeButton]}>
-                  <Text style={styles.modalButtonText}>Close</Text>
-                </View>
-              </TouchableWithoutFeedback>
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
+        <View style={styles.fullscreen}>
+          <MapView
+            style={StyleSheet.absoluteFill}
+            initialRegion={region}
+            onRegionChangeComplete={newRegion => setRegion(newRegion)}
+          >
+        </MapView>
+        <Image
+          source={require('../../assets/marker.png')}
+          style={styles.marker}
+        />
 
+          <SafeAreaView style={styles.footer}>
+          <Text style={styles.region}>
+            {`Lat: ${region.latitude.toFixed(5)}, Lon: ${region.longitude.toFixed(5)}`}
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setShowLocationPicker(false);
+              setSelectedLocation({
+                latitude: region.latitude,
+                longitude: region.longitude,
+              }); 
+            }}
+          >
+            <Text style={styles.buttonText}>Confirm Selection</Text>
+          </TouchableOpacity>
+
+        </SafeAreaView>
+        </View>
+      </Modal>
       <View style={styles.actionContainer}>
         <Text style={styles.actionText} onPress={() => setShowCategoryPicker(true)}>
           Select Event Category
@@ -300,19 +335,19 @@ const AddNewEvent = () => {
             <View style={styles.modalView}>
               <Text style={styles.modalTitle}>Select Category</Text>
               <FlatList
-                    data={categoryOptions}
-                    keyExtractor={(item) => item.id}
-                    renderItem={({ item }) => (
-                      <TouchableWithoutFeedback
-                        onPress={() => {
-                          setCategoryId(item.id);
-                          setShowCategoryPicker(false);
-                        }}
-                      >
-                        <Text style={styles.item}>{item.name}</Text>
-                      </TouchableWithoutFeedback>
-                    )}
-                  />
+                data={categoryOptions}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                  <TouchableWithoutFeedback
+                    onPress={() => {
+                      setCategoryId(item.id);
+                      setShowCategoryPicker(false);
+                    }}
+                  >
+                    <Text style={styles.item}>{item.name}</Text>
+                  </TouchableWithoutFeedback>
+                )}
+              />
               <TouchableWithoutFeedback onPress={() => setShowCategoryPicker(false)}>
                 <View style={[styles.modalButton, styles.closeButton]}>
                   <Text style={styles.modalButtonText}>Close</Text>
@@ -322,7 +357,6 @@ const AddNewEvent = () => {
           </View>
         </TouchableWithoutFeedback>
       </Modal>
-
       <TouchableOpacity onPress={handleImagePick}>
         <View style={styles.actionContainer}>
           <Text style={styles.actionText}>Pick Image</Text>
@@ -330,12 +364,10 @@ const AddNewEvent = () => {
       </TouchableOpacity>
       {renderImage()}
       {image && (
-      <TouchableOpacity style={styles.clearButton} onPress={clearImage}>
-        <Text style={styles.clearButtonText}>Clear Image</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.clearButton} onPress={clearImage}>
+          <Text style={styles.clearButtonText}>Clear Image</Text>
+        </TouchableOpacity>
       )}
-
-
       <TouchableOpacity onPress={handleSubmit}>
         <View style={styles.submitButton}>
           <Text style={styles.buttonText}>Submit Event</Text>
@@ -348,15 +380,14 @@ const AddNewEvent = () => {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    padding: 20,
+    padding: 16,
     backgroundColor: '#fff',
-    alignItems: 'left',
-    justifyContent: 'left',
   },
   header: {
-    fontSize: 24,
+    fontSize: 26,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 16,
+    textAlign: 'center',
     color: '#333',
   },
   label: {
@@ -372,58 +403,57 @@ const styles = StyleSheet.create({
   },
   clearButtonText: {
     color: '#fff',
-    textAlign: 'center',
+    textAlign: 'left',
     fontWeight: 'bold',
   },
   input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    paddingVertical: 12,
-    paddingHorizontal: 15,
-    marginBottom: 20,
-    width: '100%',
+    borderWidth: 2,
+    borderColor: '#ddd',
+    borderRadius: 12,
+    padding: 12,
+    marginBottom: 10,
+    fontSize: 16, // Smaller text
     backgroundColor: '#f9f9f9',
-    fontSize: 16,
-    color: '#333',
   },
   inputMultiline: {
-    borderWidth: 1,
-    borderColor: '#ccc',
+    borderWidth: 2,
+    borderColor: '#ddd',
     borderRadius: 8,
-    padding: 15,
-    marginBottom: 20,
-    minHeight: 150,
+    padding: 12,
+    minHeight: 100,
+    textAlign: 'top',
     width: '100%',
     backgroundColor: '#f9f9f9',
+    marginBottom: 12,
     fontSize: 16,
     color: '#333',
   },
   switchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 20,
   },
   actionContainer: {
     width: '100%',
-    marginBottom: 20,
   },
   actionText: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
-    color: '#3498db',
     marginBottom: 10,
+    marginTop: 10,
+    color: '#007bff',
   },
   selectedText: {
     fontSize: 16,
+    marginTop: 10,
     marginBottom: 10,
-    color: '#333',
+    color: '#495057',
   },
   image: {
     width: '100%',
-    height: 200,
+    height: 150,
     borderRadius: 8,
-    marginBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
     borderColor: 'red',
     borderWidth: 2,
     backgroundColor: 'lightgrey',
@@ -435,15 +465,19 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.5)',
   },
   modalView: {
-    backgroundColor: '#fff',
-    borderRadius: 12,
-    elevation: 5,
-    padding: 25,
-    alignItems: 'center',
-    maxHeight: '60%',
-    alignSelf: 'center',
-    marginTop: 'auto',
-    marginBottom: 'auto',
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
   },
   modalTitle: {
     fontSize: 20,
@@ -480,8 +514,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   closeButton: {
-    backgroundColor: '#bdc3c7',
+    backgroundColor: '#aaa', 
     marginTop: 10,
+  },
+  cancelButtonText: {
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+    padding: 10,
   },
   submitButton: {
     marginTop: 20,
@@ -492,11 +532,75 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 20,
   },
+  fullscreen: {
+    flex: 1,
+  },
+  marker: {
+    height: 50, 
+    width: 50,  
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginLeft: -25, 
+    marginTop: -50, 
+  },
+  footer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    bottom: 0,
+    position: 'absolute',
+    width: '100%',
+  },
+  locationContainer: {
+    padding: 12,
+    borderRadius: 8,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    alignSelf: 'center',
+    marginVertical: 8,
+  },
+  locationText: {
+    fontSize: 16,
+    color: '#333',
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 15, // Add some margin below the location text
+    borderRadius: 8,
+    backgroundColor: '#f9f9f9', // Give it a distinct background if needed
+    textAlign: 'center',
+  },
+  region: {
+    color: '#fff',
+    paddingTop: 18,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#27ae60', // A pleasant green color
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    marginVertical: 20,
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '80%',
+    borderRadius: 30, // Rounded corners
+    marginHorizontal: 50, // Center the button by providing horizontal margins
+    shadowColor: '#000', // Shadow for button to make it pop a bit
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
   buttonText: {
+    backgroundColor: '#4CAF50',
+    borderRadius: 20,
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
   },
+  
 });
 
 export default AddNewEvent;
